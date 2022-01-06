c564144cf99e78dc6914c996d6d40beeacd41b79
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-06 17:25:02 +0900

    basic login works

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < Devise::RegistrationsController
+
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :name, :password, :password_cofirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :name, :password, :password_cofirmation, :current_password)
+  end
+
+end

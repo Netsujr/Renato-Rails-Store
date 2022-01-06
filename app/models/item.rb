1041d8939788e3a3abed8b7e0625b048d08815fd
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-06 22:07:31 +0900

    gettin syntax erros

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
 
-mount_uploader :image, ImageUploader
-serialize :image, JSON
-belongs_to :user, optional: true
+  mount_uploader :image, ImageUploader
+  serialize :image, JSON
+  belongs_to :user, optional: true
 
-validates :title, :price, :condition, presence: true
-validates :description, length: { maximum: 500, too_long: '%{count} maximum allowed characters.' }
-validates :title, length: { maximum: 140, too_long: '%{count} maximum allowed characters.' }
-validates :price, numericality: { only_integer: true }, length: { maximum: 10 }
+  # validates :title, :price, :condition, presence: true
+  validates :description, presence: true
+  validates :title, presence: true
+  validates :condition, presence: true
+  validates :price, length: { maximum: 10 }, presence: true
 
-CONDITION = %w{ New Great Used }
+  CONDITION = ['New', 'Great', 'Used']
 
 end

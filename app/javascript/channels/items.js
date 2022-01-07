85fabe9dcca5831abbb3a14c80384b754aaa88e8
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-07 19:41:46 +0900

    image is now displaying.. which is good

---
+++ app/javascript/channels/items.js
@@ -0,0 +1,33 @@
+document.addEventListener('turbolinks:load'), function () {
+
+  const itemImage = document.querySelector('.item_image');
+
+  function handleFileSelect(event) {
+    const files = event.target.files;
+
+    for (let i = 0; f; f = files[i]; i++) {
+      if (!f.type.match('image.**')) {
+        continue;
+      }
+      const reader = new FileReader();
+
+      reader.onload = (function (theFile) {
+        return function (e) {
+          const span = document.createElement('span');
+          span.innerHTML = ['<img class="item-preview-thumb" src="', e.target.result,
+        '" title="', escape(theFile.name), '"/>'].join('');
+        document.getElementById('list').insertBefore(span, null);
+        };
+      })(f);
+      reader.readAsDataURL(f);
+    }
+  }
+
+  if (itemImage) {
+    this.addEventListener('change', handleFileSelect, false);
+  }
+
+
+
+
+}

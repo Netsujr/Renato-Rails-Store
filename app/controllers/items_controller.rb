d6b9182cc58375742495abdc78508b5ac704b12f
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-11 22:49:51 +0900

    added conditional to check if item was deleted or not

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,10 +49,14 @@ class ItemsController < ApplicationController
 
   # DELETE /items/1 or /items/1.json
   def destroy
-    @item.destroy
-    
+    @item = Item.find(params[:id])
+
     respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully deleted.' }
+      if @item.destroy
+        format.html { redirect_to root_path, notice: 'Item was successfully deleted.' }
+      else
+        format.html { redirect_to root_path, notice: 'Item wasnt deleted...' }
+      end
       format.json { head :no_content }
     end
   end

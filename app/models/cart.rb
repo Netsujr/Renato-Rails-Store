57d09c5dd3d2a5b5f9a33710940c0cf6bddcebad
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-11 15:02:23 +0900

    move methods from application into cart controller

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,4 +10,17 @@ class Cart < ApplicationRecord
     end
     current_item
   end
+
+  def total_price
+    cart_items.to_a.sum { |item| item.total_price }
+  end
+
+  def cart_count_over_one
+    return total_cart_items if total_cart_items.positive?
+  end
+
+  def total_cart_items
+    total = cart_items.map(&:quantity).sum
+    return total if total.positive?
+  end
 end

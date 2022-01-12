a4f1e5e5118312af4925a047c9fbfc65da73309b
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-12 16:43:36 +0900

    cant sign out / update item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,9 +36,12 @@ class ItemsController < ApplicationController
 
   # PATCH/PUT /items/1 or /items/1.json
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to item_url(@item), notice: 'Item was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit, status: :unprocessable_entity }

e75d6ebc92c076a4169196171ccdc3322cbe6e54
Renato Delboni <renatonato_jr@hotmail.com>
2022-01-15 19:59:50 +0900

    unique styles done!

--- app/javascript/packs/global.js
+++ app/javascript/packs/global.js
@@ -9,3 +9,19 @@ document.addEventListener("turbolinks:load", function () {
     }, 3000);
   }
 });
+
+
+// function showImage(input) {
+//   if (input.files && input.files[0]) {
+//     var reader = new FileReader();
+
+//     reader.onload = function (e) {
+//       $('#uploaded_image')
+//         .attr('src', e.target.result)
+//         .width(150)
+//         .height(200);
+//     };
+
+//     reader.readAsDataURL(input.files[0]);
+//   }
+// }
